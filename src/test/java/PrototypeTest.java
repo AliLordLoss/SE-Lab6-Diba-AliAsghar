68984e08399112cc7c6d3212188c474119979845
AliLordLoss <a.a.ghanati@gmail.com>
2023-05-12 13:58:18 +0330

    -added tests for prototype

---
+++ src/test/java/PrototypeTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+public class PrototypeTest {
+    @Test
+    void testPlanePrototype() {
+        Prototype plane = new PlanePrototype("F14");
+        Prototype planeCopy = plane.clone();
+
+        Assertions.assertTrue(!Objects.equals(plane, planeCopy) && plane.getName().equals(planeCopy.getName()));
+    }
+}
